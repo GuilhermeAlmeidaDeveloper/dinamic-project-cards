adf6314eba8957da858889cc70caaf80b782f212
Guilherme Almeida <guilherme.dev.almeida@gmail.com>
2023-03-31 01:50:11 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
+import UserCard from './components/UserCard/UserCard';
+
+const users = [
+  {
+
+    nome: 'Kelvin Costa',
+    instagram: '@costa',
+    seguidores: 140,
+    seguindo: 270
+  },
+  {
+    nome: 'Charles Santos',
+    instagram: '@charles.santos',
+    seguidores: 302,
+    seguindo: 419
+  },
+  {
+
+    nome: 'Anna Bia',
+    instagram: '@anab',
+    seguidores: 842,
+    seguindo: 150
+  },
+  {
+    nome: 'Mario Hisashi',
+    instagram: '@hisashi',
+    seguidores: 28,
+    seguindo: 17
+  }
+]
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div className="conteiner">
+
+      {users.map((user) => {
+        <UserCard
+          nome={user.name}
+          instagram={user.name}
+          seguidores={user.seguidores}
+          seguindo={user.seguindo}
+        />
+      })}
+
+
     </div>
-  );
+  )
+
+
+
 }
 
 export default App;
---
+++ src/components/Avatar/index.js
@@ -0,0 +1,11 @@
+import './styles.css'
+
+function Avatar() {
+    return (
+        <div className='avatariano'>
+            <img></img>
+        </div>
+    )
+}
+
+export default Avatar
